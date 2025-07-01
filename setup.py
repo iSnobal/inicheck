2b84eece1c02c80e1e11533d616611997f79e0c7
Micah Sandusky <micc5725@gmail.com>
2025-07-01 10:58:22 -0600

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('requirements.txt') as req_file:
 
 setup(
     name='m3w-inicheck',
-    version='0.2.1',
+    version='0.2.2',
     description="inicheck is an high level configuration file checker "
                 "enabling developers tight control over their users "
                 "configuration files",
